8af918c2c148cc9bf6da51fd42c9479daf5b70de
emiliaverdugo <verdugovega.e@gmail.com>
2022-06-27 19:24:05 +0000

    cdf comentado

--- customPDFs.py
+++ customPDFs.py
@@ -117,64 +117,7 @@ class bernstein(zfit.pdf.BasePDF):
 
         return pdf
 ################cdf 
-    def __getstate__(self):
-        dct = self.__dict__.copy()
-
-        # these methods will be remade in __setstate__
-        # _random_state attribute is taken care of by rv_generic
-        attrs = ["_parse_args", "_parse_args_stats", "_parse_args_rvs",
-                 "_cdfvec", "_ppfvec", "vecentropy", "generic_moment"]
-        [dct.pop(attr, None) for attr in attrs]
-        return dct
-
-      
-    def _cdf_single(self, x, *args):
-        _a, _b = self._get_support(*args)
-        return integrate.quad(self.unnormalized_pdf, _a, x, args=args)[0]
-
-    def _cdf(self, x, *args):
-        return self._cdfvec(x, *args)
-
-    def cdf(self, x, *args, **kwds):
-        """
-        Cumulative distribution function of the given RV.
-        Parameters
-        ----------
-        x : array_like
-            quantiles
-        arg1, arg2, arg3,... : array_like
-            The shape parameter(s) for the distribution (see docstring of the
-            instance object for more information)
-        loc : array_like, optional
-            location parameter (default=0)
-        scale : array_like, optional
-            scale parameter (default=1)
-        Returns
-        -------
-        cdf : ndarray
-            Cumulative distribution function evaluated at `x`
-        """
-        args, loc, scale = self._parse_args(*args, **kwds)
-        x, loc, scale = map(asarray, (x, loc, scale))
-        args = tuple(map(asarray, args))
-        _a, _b = self._get_support(*args)
-        dtyp = np.find_common_type([x.dtype, np.float64], [])
-        x = np.asarray((x - loc)/scale, dtype=dtyp)
-        cond0 = self._argcheck(*args) & (scale > 0)
-        cond1 = self._open_support_mask(x, *args) & (scale > 0)
-        cond2 = (x >= np.asarray(_b)) & cond0
-        cond = cond0 & cond1
-        output = zeros(shape(cond), dtyp)
-        place(output, (1-cond0)+np.isnan(x), self.badvalue)
-        place(output, cond2, 1.0)
-        if np.any(cond):  # call only if at least 1 entry
-            goodargs = argsreduce(cond, *((x,)+args))
-            place(output, cond, self._cdf(*goodargs))
-        if output.ndim == 0:
-            return output[()]
-        return output
-    
-    # def cdf(self,x):
+       # def cdf(self,x):
         # min_=self.obs.limits[0]
         # cdf= quad(self.pdf,min_,x)
         # return cdf
